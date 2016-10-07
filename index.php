99ad98b0e41c1474010a6f3b5c12cff9dbb8bede
ADSYSAMA <adsysama@gmail.com>
2016-10-07 14:32:11 +0200

    inch ca marche

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+
+<html>
+
+
+  <head>
+    <meta charset="utf-8">
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+  </head>
+
+<body>
+
+  <div class="background">
+    <img style="margin-left:570px; margin-top:180px;" src="images/circle.png"/>
+    <h1 class="name">ADSY SAMA</h1>
+    <h2 class="info">STUDENT BASED IN PARIS</h2>
+  </div>
+
+  <div class="social"> 
+    <a href="https://www.facebook.com/adrien.simon06"><img src="images/svg/social-1_logo-facebook.svg"></a>
+    <a href="https://www.linkedin.com/in/adrien-simon-1b556b110?"><img src="images/svg/social-1_logo-linkedin.svg"></a>  
+    <a href="https://twitter.com/ADSY_SAMA"><img src="images/svg/social-1_square-twitter.svg"></a> 
+  </div>
+
+    <a class="mail" href="mailto:adrien.simon@edu.esiee.fr">adrien.simon@edu.esiee.fr</a>
+
+  
+    
+</body>
+  <script src="bootstrap/js/jquery.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+  <script>
+    
+  </script>
+</html>
